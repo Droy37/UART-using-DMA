40dc5e269ef32b43940a7897cd0bf7980c0893ea
Droy37 <catherine.cao37@gmail.com>
2024-10-05 12:18:17 +0800

    forwarding function

---
+++ cmake-build-debug-mingw-stm32/CMakeFiles/DMA_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/DMA_test.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj: \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_tim.c \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ C:\CLionProjects\DMA_test\Core\Inc/stm32f4xx_hal_conf.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f427xx.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Include/core_cm4.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/features.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_newlib_version.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_intsup.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_stdint.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Include/cmsis_version.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Include/cmsis_compiler.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Include/cmsis_gcc.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Include/mpu_armv7.h \
+ C:\CLionProjects\DMA_test\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stddef.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ C:\CLionProjects\DMA_test\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h
